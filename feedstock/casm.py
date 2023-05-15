21b449c03ad9f853381f9ff7402fa34b86c3d885
Julius Busecke <julius@ldeo.columbia.edu>
2023-05-15 14:24:50 -0400

    Extend to full time

--- feedstock/casm.py
+++ feedstock/casm.py
@@ -6,8 +6,7 @@ from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
 
 
-# years = range(2002, 2021)
-years = range(2002, 2005)
+years = range(2002, 2021)
 
 input_urls = [
     f"https://zenodo.org/record/7072512/files/CASM_SM_{year}.nc" for year in years]
