088f285500e2779a81b114273fb62ed019df7c00
jbusecke <julius@ldeo.columbia.edu>
2023-05-15 12:45:57 -0400

    Add CASM recipe

---
+++ feedstock/casm.py
@@ -0,0 +1,24 @@
+"""
+TODO: Add docstring
+"""
+import apache_beam as beam
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
+
+
+# years = range(2002, 2021)
+years = range(2002, 2005)
+
+input_urls = [
+    f"https://zenodo.org/record/7072512/files/CASM_SM_{year}.nc" for year in years]
+
+pattern = pattern_from_file_sequence(input_urls, concat_dim='date')
+CASM = (
+    beam.Create(pattern.items())
+    | OpenURLWithFSSpec()
+    | OpenWithXarray()
+    | StoreToZarr(
+        store_name="CASM.zarr",
+        combine_dims=pattern.combine_dim_keys,
+    )
+)
