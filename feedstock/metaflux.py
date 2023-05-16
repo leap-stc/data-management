dd72f7b146dcb50d957d0183c3985b3fbc56509e
Julius Busecke <julius@ldeo.columbia.edu>
2023-05-16 19:12:09 -0400

    Update metaflux.py

--- feedstock/metaflux.py
+++ feedstock/metaflux.py
@@ -9,8 +9,7 @@ from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
 
 # Common Parameters
-# years = range(2001, 2022)
-years = range(2008, 2022) # for debugging
+years = range(2001, 2022)
 months = range(1, 13)
 dataset_url = 'https://zenodo.org/record/7761881/files'
 
