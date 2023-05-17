b88619e7becbd274c55620c3ce28bf3473aed307
Julius Busecke <julius@ldeo.columbia.edu>
2023-05-16 20:12:56 -0400

    Add CASM recipe (#22)
    * Add CASM recipe
    
    * Extend to full time
    
    * Update casm.py
    
    * Testing 2012 to 2020
    
    * 2006-end
    
    * 2008-end
    
    * Fix pgf-recipes version
    
    * full time range + fixed pgf depenency
    
    * provide target chunks
    
    * Update meta.yaml

---
+++ feedstock/casm.py
@@ -0,0 +1,23 @@
+"""
+TODO: Add docstring
+"""
+import apache_beam as beam
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
+
+years = range(2002, 2021)
+
+input_urls = [
+    f"https://zenodo.org/record/7072512/files/CASM_SM_{year}.nc" for year in years]
+
+pattern = pattern_from_file_sequence(input_urls, concat_dim='date')
+CASM = (
+    beam.Create(pattern.items())
+    | OpenURLWithFSSpec()
+    | OpenWithXarray()
+    | StoreToZarr(
+        target_chunks={'date':20},
+        store_name="CASM.zarr",
+        combine_dims=pattern.combine_dim_keys,
+    )
+)
