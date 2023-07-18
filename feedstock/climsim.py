31a7354bb00b26bc1f97df06b08f062ca13e7064
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-07-18 14:09:00 -0700

    pre-commit fixes

--- feedstock/climsim.py
+++ feedstock/climsim.py
@@ -1,12 +1,10 @@
 import apache_beam as beam
 import pandas as pd
-
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
 
-
-var_names = ["mli", "mlo"]
-times = pd.date_range("0001-02-01", "0009-02-01", freq="1200S", unit="s", inclusive="left")
+var_names = ['mli', 'mlo']
+times = pd.date_range('0001-02-01', '0009-02-01', freq='1200S', unit='s', inclusive='left')
 
 
 def make_url(t: pd.Timestamp, var_name: str):
@@ -17,14 +15,14 @@ def make_url(t: pd.Timestamp, var_name: str):
     """
     seconds = (t.hour * 3600) + (t.minute * 60)
     return (
-        "https://huggingface.co/datasets/LEAP/ClimSim_high-res/resolve/main/train/"
-        f"{t.year:04}-{t.month:02}/E3SM-MMF.{var_name}."
-        f"{t.year:04}-{t.month:02}-{t.day:02}-{seconds:05}.nc"
+        'https://huggingface.co/datasets/LEAP/ClimSim_high-res/resolve/main/train/'
+        f'{t.year:04}-{t.month:02}/E3SM-MMF.{var_name}.'
+        f'{t.year:04}-{t.month:02}-{t.day:02}-{seconds:05}.nc'
     )
 
 
-concat_dim = ConcatDim("t", keys=times)
-merge_dim = MergeDim("var", keys=var_names)
+concat_dim = ConcatDim('t', keys=times)
+merge_dim = MergeDim('var', keys=var_names)
 pattern = FilePattern(make_url, concat_dim, merge_dim)
 
 climsim = (
@@ -32,7 +30,7 @@ climsim = (
     | OpenURLWithFSSpec()
     | OpenWithXarray()
     | StoreToZarr(
-        target_chunks={"time": 20},
+        target_chunks={'time': 20},
         store_name='climsim-highres-train.zarr',
         combine_dims=pattern.combine_dim_keys,
     )
