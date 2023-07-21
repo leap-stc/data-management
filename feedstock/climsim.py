9cc5d1ec5bde33ab7c02a447f0d86d48d846e2dd
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-07-20 23:12:39 -0700

    add preproc transform

--- feedstock/climsim.py
+++ feedstock/climsim.py
@@ -2,7 +2,13 @@ import datetime as dt
 
 import apache_beam as beam
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, MergeDim
-from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
+from pangeo_forge_recipes.transforms import (
+    Indexed,
+    OpenURLWithFSSpec,
+    OpenWithXarray,
+    StoreToZarr,
+    T,
+)
 
 var_names = ['mli', 'mlo']
 
@@ -21,29 +27,63 @@ def generate_times():
 
     start = dt.datetime(year=1, month=2, day=1, minute=0)
     delta = dt.timedelta(minutes=20)
+    # FIXME: calendar is noleap, remove extra feb day from leap year
     for i in range(210_384):  # means the last value yielded is `dt.datetime(9, 1, 31, 23, 40)`
         yield start + (delta * i)
 
 
-def make_url(t: dt.datetime, var: str):
+def make_url(time: dt.datetime, var: str):
     """Given a datetime and variable name, return a url pointing to the corresponding NetCDF file.
 
     For example, the inputs ``(dt.datetime(1, 2, 1, 0, 20), "mli")`` will return:
     https://huggingface.co/datasets/LEAP/ClimSim_high-res/resolve/main/train/0001-02/E3SM-MMF.mli.0001-02-01-01200.nc
     """
-    seconds = (t.hour * 3600) + (t.minute * 60)
+    seconds = (time.hour * 3600) + (time.minute * 60)
     return (
         'https://huggingface.co/datasets/LEAP/ClimSim_high-res/resolve/main/train/'
-        f'{t.year:04}-{t.month:02}/E3SM-MMF.{var}.'
-        f'{t.year:04}-{t.month:02}-{t.day:02}-{seconds:05}.nc'
+        f'{time.year:04}-{time.month:02}/E3SM-MMF.{var}.'
+        f'{time.year:04}-{time.month:02}-{time.day:02}-{seconds:05}.nc'
     )
 
 
 times = [t for t in generate_times()]
 
-concat_dim = ConcatDim('t', keys=times)
+concat_dim = ConcatDim('time', keys=times)
 merge_dim = MergeDim('var', keys=var_names)
 pattern = FilePattern(make_url, concat_dim, merge_dim)
+
+
+class ExpandTimeDimAndRenameVars(beam.PTransform):
+    """ """
+
+    @staticmethod
+    def _preproc(item: Indexed[T]) -> Indexed[T]:
+        """"""
+        index, ds = item
+
+        # FIXME: what's a better format to parse this into?
+        timestamp = str(ds.ymd.values) + str(ds.tod.values)
+
+        ds = ds.expand_dims(time=[timestamp])
+
+        overlapping = [
+            'ymd',
+            'tod',
+            'state_q0001',
+            'state_q0002',
+            'state_q0003',
+            'state_t',
+            'state_u',
+            'state_v',
+        ]
+        ds_type = 'mli' if 'cam_in_ALDIF' in ds.data_vars else 'mlo'
+        rename = {vname: f'{ds_type}_{vname}' for vname in overlapping}
+        ds = ds.rename(rename)
+        return index, ds
+
+    def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
+        return pcoll | beam.Map(self._preproc)
+
 
 # FIXME:
 #  - remove tempdir stuff here and in pipeline (this is for local debugging)
@@ -63,6 +103,7 @@ climsim = (
         copy_to_local=True,
         xarray_open_kwargs=dict(engine='netcdf4'),
     )
+    | ExpandTimeDimAndRenameVars()
     | StoreToZarr(
         target_root=td.name,  # FIXME: remove
         target_chunks={'time': 20},
