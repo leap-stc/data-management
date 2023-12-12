e5e18488304b8df3c946e63649f57bbebce61482
Julius Busecke <julius@ldeo.columbia.edu>
2023-12-11 20:12:48 -0500

    fix pattern and concat dim

--- feedstock/eNATL60-feedstock/eNATL60.py
+++ feedstock/eNATL60-feedstock/eNATL60.py
@@ -12,20 +12,19 @@ dataset_url = 'https://zenodo.org/records/10261274/files'
 
 ## Monthly version
 input_urls = [f'{dataset_url}/eNATL60-BLBT02_y2009m07d{d:02d}.1d_TSW_60m.nc' for d in days]
-pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
+pattern = pattern_from_file_sequence(input_urls, concat_dim='time_counter')
 
 # does this succeed with all coords stripped?
 class Preprocess(beam.PTransform):
     @staticmethod
     def _set_coords(item: Indexed[T]) -> Indexed[T]:
         index, ds = item
-        ds.set_coords(['depthw', 'nav_lon', 'nav_lat'])
+        ds = ds.set_coords(['depthw', 'nav_lon', 'nav_lat'])
         return index, ds
 
     def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
         return pcoll | 'Set coordinates' >> beam.Map(self._set_coords)
 
-pattern_monthly = pattern_from_file_sequence(input_urls, concat_dim='time_counter')
 eNATL60_BLBT02 = (
     beam.Create(pattern.items())
     | OpenURLWithFSSpec()
