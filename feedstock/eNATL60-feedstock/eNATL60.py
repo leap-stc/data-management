5e347ec92f064bf3382d51313cecc98e874ca0ff
Julius Busecke <julius@ldeo.columbia.edu>
2023-12-11 19:57:59 -0500

    use lambda function for preprocessing

--- feedstock/eNATL60-feedstock/eNATL60.py
+++ feedstock/eNATL60-feedstock/eNATL60.py
@@ -15,16 +15,11 @@ input_urls = [f'{dataset_url}/eNATL60-BLBT02_y2009m07d{d:02d}.1d_TSW_60m.nc' for
 pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
 
 
-def preprocess(ds: xr.Dataset) -> xr.Dataset:
-    ds = ds.set_coords(['depthw', 'nav_lon', 'nav_lat'])
-    return ds
-
-
 pattern_monthly = pattern_from_file_sequence(input_urls, concat_dim='time_counter')
 eNATL60_BLBT02 = (
     beam.Create(pattern.items())
     | OpenURLWithFSSpec()
-    | OpenWithXarray(xarray_open_kwargs={'preprocess': preprocess})
+    | OpenWithXarray(xarray_open_kwargs={'preprocess':lambda ds: ds.set_coords(['depthw', 'nav_lon', 'nav_lat'])})
     | StoreToZarr(
         store_name='eNATL60_BLBT02.zarr',
         combine_dims=pattern.combine_dim_keys,
