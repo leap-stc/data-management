703083191d398d87b6127bf503ff5f8716fcebb3
bismi <bismillakhans@gmail.com>
2024-10-04 00:27:47 +0530

    Update casm.py
    comment added

--- feedstock/casm.py
+++ feedstock/casm.py
@@ -7,7 +7,7 @@ from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
 
 years = range(2002, 2021)
-
+# create a list of input urls from zenodo.org based on the year
 input_urls = [f'https://zenodo.org/record/7072512/files/CASM_SM_{year}.nc' for year in years]
 
 pattern = pattern_from_file_sequence(input_urls, concat_dim='date')
